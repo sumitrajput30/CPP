434ee30b95ebfc00e4f432e86a5aa2ebc1f22f6a
sumit <sumitrajput88956@gmail.com>
2025-02-03 12:44:40 +0530

    String

---
+++ String/Q.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s={"subh"};
+    s="laabh";
+    cout<<s.length()<<"\n";
+    string t={"yes"};
+    cout<<(s<t)<<"\n";
+    cout<<s;
+}
