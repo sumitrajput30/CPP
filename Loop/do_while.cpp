abf7f4d121d85b667ddafcf53821f6f163d4d182
sumit <sumitrajput88956@gmail.com>
2025-02-08 00:32:14 +0530

    Loop

---
+++ Loop/do_while.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    do{
+        if(i%2==0){
+             cout<<i<<"\n";
+        }
+        i++;
+    }while(i<=10);
+}
+
+
+
