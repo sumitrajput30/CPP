e361b1aef61da2c9ba112121b99fef05f16e5f83
sumit <sumitrajput88956@gmail.com>
2025-02-09 14:49:19 +0530

    if and else

---
+++ if else condition/if_else.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter first no\n";
+    cin>>a;
+    cout<<"enter second no\n";
+    cin>>b;
+    if(a>b)
+    {
+        cout<<"large no"<<a;
+
+    }
+    else
+    {
+        cout<<"large no"<<b;
+    }
+
+}
