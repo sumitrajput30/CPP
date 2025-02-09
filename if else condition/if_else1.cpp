b904c0d77e6747e3f807d043ec21a5704f08fc94
sumit <sumitrajput88956@gmail.com>
2025-02-09 14:51:35 +0530

    if and else

---
+++ if else condition/if_else1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout<<"enter any alpha";
+    cin>>c;
+    if(c=='a' || c=='e' || c=='i'||c=='o'||c=='u');
+    {
+    cout<<"it is vowel";
+    }
+    else
+    {
+    cout<<"it is consonant";
+    }
+}
