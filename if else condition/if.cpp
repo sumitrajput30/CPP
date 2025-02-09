e24c1c4ea2043e1a927f83ef6b0b2ddaacfcd65c
sumit <sumitrajput88956@gmail.com>
2025-02-09 14:48:24 +0530

    if

---
+++ if else condition/if.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter u g\n";
+    cin>>a;
+
+    if(a=='m'||a=='M');
+    {
+    cout<<male;
+
+    }
+
+    if(a=='f' || a=='F')
+    {
+    cout<<female;
+    }
+    else
