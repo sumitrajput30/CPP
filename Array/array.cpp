e9c468dfb09426e6355f7fd2a56900cd8d888c9a
sumit <sumitrajput88956@gmail.com>
2025-02-11 12:07:12 +0530

    array

---
+++ Array/array.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int s;
+    cout<<"enter the size of array\n";
+    cin>>s;
+    int a[s];
+    cout<<"value\n";
+    for(int i=0;i<s;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"output\n";
+    for(int i=0;i<s;i++)
+    {
+
+
+        for(int j=i;j<s-1;j++)
+    {
+        if(a[i] == a[j+1])
+        {
+            cout<<a[i];
+        }
+
+    }
+
+    }
+
+}
