b8fec4df17013c38278ef31d6fe43b4292b4d91c
sumit <sumitrajput88956@gmail.com>
2025-02-14 02:36:48 +0530

    Function

---
+++ OOPS/function1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class student
+{
+   int a=10;
+   public:void show()
+   {
+       cout<<a;
+   }
+};
+int main()
+
+{
+    student obj;
+    obj.show();
+}
