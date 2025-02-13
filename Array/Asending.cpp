2bc1eb64596d52ddc199a8386fa0766cc1e1bf2d
sumit <sumitrajput88956@gmail.com>
2025-02-13 14:11:15 +0530

    Asending

---
+++ Array/Asending.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+
+    int s;
+    cout<<"enter no array\n";
+    cin>>s;
+    int a[s];
+    int t;
+    cout<<"enter value\n";
+
+    for(int i=0;i<s;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<s;i++)
+    {
+        cout<<a[i];
+    }
+     for(int i=0;i<s;i++)
+    {
+    for (int j=i;j<s-1;j++)
+    {
+        if (a[i]<a[j+1])
+        {
+          t=a[i];
+          a[i]=a[j];
+          a[j]=t;
+        }
+    }
+
+
+    }
+
+    cout<<"\nasc order=\n";
+    for (int i=0;i<s;i++)
+    {
+        cout<<a[i]<<"\t";
+    }
+}
