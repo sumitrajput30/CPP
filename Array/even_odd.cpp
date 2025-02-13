1292b6e97342e5be2cb3ee2448bbc978bbc6e15e
sumit <sumitrajput88956@gmail.com>
2025-02-13 14:14:33 +0530

    Even And Odd

---
+++ Array/even_odd.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int a[5];
+
+
+    for (int i=0;i<5;i++)
+    {
+        cin>>a[i];
+    }
+      for (int i=0;i<5;i++)
+      {
+
+
+        if (a[i]%2==0)
+        {
+
+
+            cout<<a[i];
+        }
+      }
+
+
+
+}
