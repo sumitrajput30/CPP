4e6689911a55c18149cafccf17507e4a4796b800
sumit <sumitrajput88956@gmail.com>
2025-02-23 01:17:03 +0530

    pattern in increasing order

---
+++ Pattern/Pattern_in_increasing_order.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{  int a=1;
+    for(int row=1; row<=3;row++)
+    {
+        for(int col=1; col<=row; col++)
+        {
+
+            cout<<a<<"  ";
+            a++;
+
+        }
+
+    cout<<"\n";
+    }
+}
