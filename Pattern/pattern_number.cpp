cd3d3ecf815a9479e9d4a2e483a5f2dcf1bd957e
sumit <sumitrajput88956@gmail.com>
2025-02-23 01:15:51 +0530

    No Pattern

---
+++ Pattern/pattern_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    for(int row=1; row<=3;row++)
+    {
+        for(int col=1; col<=row; col++)
+        {
+
+            cout<<row<<"  ";
+
+        }
+
+    cout<<"\n";
+    }
+}
