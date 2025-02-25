042f8cbcc39c97e3cc8f7bcd955ef8af6a4b9719
sumit <sumitrajput88956@gmail.com>
2025-02-25 12:27:29 +0530

    Left Pattern

---
+++ Pattern/left3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+
+
+    for( int r=65;r<=68;r++)
+    {
+        for( int s=68-r;s>=1;s--)
+        {
+            cout<<"  ";
+        }
+        for(int c=65;c<=r;c++)
+        {
+            cout<< char(c)<<" ";
+
+        }
+
+        cout<<"\n";
+
+    }
+}
